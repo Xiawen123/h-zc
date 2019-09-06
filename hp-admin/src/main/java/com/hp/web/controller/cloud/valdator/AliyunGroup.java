a33b897b169e6f03d38f6b68183fe9d89d0b60e7
王功文 <223519600@qq.com>
2019-09-06 10:05:56 +0800

    修改文件服务器相关

--- hp-admin/src/main/java/com/hp/web/controller/cloud/valdator/AliyunGroup.java
+++
@@ -1,8 +0,0 @@
-package com.hp.web.controller.cloud.valdator;
-
-/**
- * 阿里云
- */
-public interface AliyunGroup
-{
-}
--- hp-admin/src/main/java/com/hp/web/controller/cloud/valdator/QcloudGroup.java
+++
@@ -1,8 +0,0 @@
-package com.hp.web.controller.cloud.valdator;
-
-/**
- * 腾讯云
- */
-public interface QcloudGroup
-{
-}
