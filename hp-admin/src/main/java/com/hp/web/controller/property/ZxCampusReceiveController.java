4c993e45b1d18dbd926ccc70f6e3b79769cf7b15
zhiheng <428308390@qq.com>
2019-09-09 09:29:07 +0800

    校区领用

--- hp-admin/src/main/java/com/hp/web/controller/property/ZxCampusReceiveController.java
+++ hp-admin/src/main/java/com/hp/web/controller/property/ZxCampusReceiveController.java
@@ -7,10 +7,16 @@ import com.hp.common.core.page.TableDataInfo;
 import com.hp.common.enums.BusinessType;
 import com.hp.common.utils.DateString;
 import com.hp.common.utils.SnowFlake;
+import com.hp.framework.util.ShiroUtils;
 import com.hp.property.domain.ZxAssetManagement;
 import com.hp.property.domain.ZxChange;
 import com.hp.property.service.IZxAssetManagementService;
 import com.hp.property.service.impl.ZxChangeServiceImpl;
+import com.hp.system.domain.SysDictData;
+import com.hp.system.domain.SysUser;
+import com.hp.system.service.ISysDeptService;
+import com.hp.system.service.ISysDictDataService;
+import com.hp.system.service.ISysUserService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -39,6 +45,15 @@ public class ZxCampusReceiveController extends BaseController {
     @Autowired(required = false)
     private IZxAssetManagementService zxAssetManagementService;
 
+    @Autowired
+    private ISysUserService iSysUserService;
+
+    @Autowired
+    private ISysDeptService iSysDeptService;
+
+    @Autowired
+    private ISysDictDataService iSysDictDataService;
+
     // 跳转到校区领用主页面
     @RequiresPermissions("property:campusrecive:view")
     @GetMapping()
@@ -65,12 +80,10 @@ public class ZxCampusReceiveController extends BaseController {
         // 查询变更记录表中，变动类型为领用的记录对应资产表的记录
         List<ZxAssetManagement> list =new ArrayList<>();
         for (ZxChange z:zxChanges){
-
             Long id = z.getAssetsId();
             zxAssetManagement.setId(id);
             list = zxAssetManagementService.selectZxAssetManagementListById(zxAssetManagement);
         }
-
         return getDataTable(list);
     }
 
@@ -85,19 +98,13 @@ public class ZxCampusReceiveController extends BaseController {
 
 
    // 新增保存资产信息
-
     @RequiresPermissions("property:campusrecive:add")
     @Log(title = "资产信息", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
     public AjaxResult addSave(ZxAssetManagement zxAssetManagement)
     {
-        // 修改资产状态：闲置变为领用
-        // zxAssetManagementService.modifyZxAssertManagement(ids);
-
         System.out.println("zxAssetManagement:_____________" + zxAssetManagement.toString());
-
-        // return toAjax(zxAssetManagementService.insertZxAssetManagement(zxAssetManagement));
 
         String ids = zxAssetManagement.getIds();
         int i1=0;
@@ -116,6 +123,21 @@ public class ZxCampusReceiveController extends BaseController {
                 zxChange.setUseDepartment(zxAssetManagement.getDepartment());
                 zxChange.setUsers(zxAssetManagement.getExtend2());
                 zxChange.setExtend1(DateString.getString(new Date(),"yyyy-MM-dd HH:mm:ss"));
+                //在变更表中存入提交人
+                zxChange.setSubmitOne(ShiroUtils.getLoginName());
+                //在变更表中存入提交人所属部门
+                SysUser sysUser = iSysUserService.selectUserByLoginName(ShiroUtils.getLoginName());
+                String c= iSysDeptService.selectDeptById(sysUser.getDeptId()).getDeptName();
+                List<SysDictData> zc_department = iSysDictDataService.selectDictDataByType("zc_department");
+                for (SysDictData sysDictData:zc_department){
+                    if (c.equals(sysDictData.getDictLabel())){
+                        int d=Integer.parseInt(sysDictData.getDictValue());
+                        zxChange.setSubmittedDepartment(d);
+                    }
+                }
+                // 在变更表中存入使用校区
+                zxChange.setExtend4(zxAssetManagement.getExtend4());
+
                 zxChangeService.insertZxChange(zxChange);
                 i1 = zxAssetManagementService.updateZxAssetManagement(zxone);
             }
