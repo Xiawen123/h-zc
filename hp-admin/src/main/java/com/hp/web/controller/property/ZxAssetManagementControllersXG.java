8dc5cdbb76e239bea59c0d1dc7a60a7a55f9f8a4
lijiajiji <lijiaji1994@outlook.com>
2019-09-10 15:17:17 +0800

    liJiaji

--- hp-admin/src/main/java/com/hp/web/controller/property/ZxAssetManagementControllersXG.java
+++ hp-admin/src/main/java/com/hp/web/controller/property/ZxAssetManagementControllersXG.java
@@ -73,7 +73,7 @@ public class ZxAssetManagementControllersXG extends BaseController
                 if (a.equals(b)) {
                     String c=sysDept1.getDeptName();
                     zxAssetManagement1.setExtend5(c);}
-                }
+            }
             }
         }
 
